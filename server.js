2db9f75edd4d20e32eb472f0eb9304a86cd4d6f3
rsami23 <roshnee.kissoon@gmail.com>
2018-06-11 10:28:22 -0400

    changed server file

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require("path");
 const app = express();
 const cors = require("cors");
 const router = require('./routes/api/beers');
+const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -38,12 +39,10 @@ mongoose.connect(
 
 // Send every request to the React app
 // Define any API routes before this runs
-// app.get("*", function(req, res) {
-  // res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-const PORT = process.env.PORT || 3001;
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+  console.log(`🌎 ==> Server now on portx ${PORT}!`);
 });
