41ea9baf87aba55d20cd4b91a2bbc251026968ea
rsami23 <roshnee.kissoon@gmail.com>
2018-06-11 22:58:03 -0400

    no more errors but no data still

---
+++ client/src/components/Data/Data.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Data.css";
+
+const Data = () => (
+    <div>
+        <div className="data">
+            <h5>Jai Alai</h5>
+            <p className="beerItems">Rating: 5</p>
+            <p className="beerItems">Beer Type: IPA</p>
+            <p className="beerItems">Brewery Name: Cigar City</p>
+            <p className="beerItems">Summary: Pours a golden color with a white foam head. Aroma is citrus, floral, and pine hops. Flavor is similar, with more floral character and some resin. Slightly bitter, a little bit sweet. Has some carbonation. A pretty good IPA.</p>   
+        </div>
+        <div className="data">
+            <h5>Jai Alai</h5>
+            <p className="beerItems">Rating: 5</p>
+            <p className="beerItems">Beer Type: IPA</p>
+            <p className="beerItems">Brewery Name: Cigar City</p>
+            <p className="beerItems">Summary: Pours a golden color with a white foam head. Aroma is citrus, floral, and pine hops. Flavor is similar, with more floral character and some resin. Slightly bitter, a little bit sweet. Has some carbonation. A pretty good IPA.</p>   
+        </div>
+        <div className="data">
+            <h5>Jai Alai</h5>
+            <p className="beerItems">Rating: 5</p>
+            <p className="beerItems">Beer Type: IPA</p>
+            <p className="beerItems">Brewery Name: Cigar City</p>
+            <p className="beerItems">Summary: Pours a golden color with a white foam head. Aroma is citrus, floral, and pine hops. Flavor is similar, with more floral character and some resin. Slightly bitter, a little bit sweet. Has some carbonation. A pretty good IPA.</p>   
+        </div>
+    </div>
+)
+
+export default Data;
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AddBeer from "../../components/AddBeer";
+import Data from "../../components/Data";
 import requireAuth from "../../requireAuth";
 import "./Dashboard.css";
 
@@ -14,6 +15,7 @@ class Dashboard extends Component {
                     <div className="col-sm-6" id="topRated">
                         <h1 className="display-4 topRatedTitle">Top Rated Brews</h1>
                         <div className="topRatedBrews">
+                            <Data />
                         </div>
                     </div>
                     <div className="col-sm-6">
